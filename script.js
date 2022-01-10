bbff23cc22a0ca2b5ed5e53738445790ada92478
XavierCoulon <xcoulon.mail@gmail.com>
2022-01-10 17:35:12 +0100

    work on fetch & PAI

--- script.js
+++ script.js
@@ -6,40 +6,40 @@ const bestMovieSection = document.querySelector(".bestmovie");
 const bestMovieDetailsSection = document.querySelector(".bestmovie-details");
 
 
-// Test
-
-// fetch("http://localhost:8000/api/v1/titles/?year=2020")
-// .then(response => response.json())
-// .then(response => response["results"])
-// .then(data => {
-//     console.log(data);
-
-//     for (i = 0; i < data.length; i++) {
-//         let newTitle = document.createElement("h1");
-//         let newImg = document.createElement("img");
-//         newTitle.innerText = data[i].title;
-//         newImg.src = data[i].image_url; 
-//         liste.appendChild(newTitle);
-//         liste.appendChild(newImg)
-//     }
-// })
-
-
-// Best movie ever and the 6 next (all categories)
-
-
-async function fetchBestMovies() {
-    let response = await fetch("http://localhost:8000/api/v1/titles/?&sort_by=-imdb_score");
-    let bestMovies = await response.json();
-    return bestMovies
+// Return Id from the API's best movie
+async function fetchBestMoviesId() {
+    try{
+        let response = await fetch("http://localhost:8000/api/v1/titles/?&sort_by=-imdb_score");
+        let data = await response.json();
+        return await data["results"][0]["id"];
+    } catch (error) {
+        console.error("Error when trying to fetch best movie");
+    }
 }
 
-async function renderBestMovie() {
-    let bestMovies = await fetchBestMovies();
-    // console.log(bestMovies["results"][0]["id"])
-    return bestMovies["results"][0]["id"]
+// Return data from a specific movie (Id)
+async function fetchMovieDetails(id) {
+    try {
+        const response = await fetch(`http://localhost:8000/api/v1/titles/${id}`, {
+            method: 'GET',
+            credentials: 'same-origin'
+        });
+        const movie = await response.json();
+        return movie;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
+// Return movies from a category (sorted by score)
+async function fetchBestMoviesByCategory(category) {
+    const response = await fetch(`http://localhost:8000/api/v1/titles/?&page_size=8&sort_by=-imdb_score&genre=${category}`);
+    const data = await response.json();
+    return await data["results"];
+}
+
+
+// Display information of best movie
 function displayBestMovie(id){
     let bestMovieImg = document.createElement("img");
     let bestMovieTitle = document.createElement("li");
@@ -52,7 +52,7 @@ function displayBestMovie(id){
     let bestMovieActors = document.createElement("li");
     let bestMovieDuration = document.createElement("li");
     
-    renderMovieDetails(id).then(movie => {   
+    fetchMovieDetails(id).then(movie => {   
         bestMovieImg.src = movie["image_url"];
         bestMovieTitle.innerText = movie["title"];
         bestMovieDescription.textContent = movie["description"];
@@ -77,36 +77,9 @@ function displayBestMovie(id){
     })
 }
 
-
-// fetch("http://localhost:8000/api/v1/titles/?&sort_by=-imdb_score")
-// .then(response => response.json())
-// .then(response => response["results"][0])
-// .then(bestMovie => {
-//     //console.log(bestMovie);
-//     let bestMovieImg = document.createElement("img");
-//     let bestMovieTitle = document.createElement("h1");
-//     bestMovieImg.src = bestMovie.image_url;
-//     bestMovieTitle.innerText = bestMovie.title;
-//     liste.appendChild(bestMovieImg);
-//     liste.appendChild(bestMovieTitle);
-//  })
-
-
-async function fetchBestMoviesByCategory(category) {
-    const response = await fetch(`http://localhost:8000/api/v1/titles/?&page_size=8&sort_by=-imdb_score&genre=${category}`);
-    const bestMovies = await response.json();
-    //console.log(bestMovies);
-    return bestMovies
-}
-
-async function renderBestMoviesByCategory(category) {
-    const movies = await fetchBestMoviesByCategory(category)
-    //console.log(movies["results"]);
-    return movies["results"];
-}
-
+// Display movies from 3 categories
 function displayBestMoviesCategory(category) {
-    renderBestMoviesByCategory(category).then(results => {
+    fetchBestMoviesByCategory(category).then(results => {
         let bestMoviesCategory = results;
         let categoryTitle = document.createElement("div");
         categoryTitle.className = "category_title";
@@ -119,7 +92,8 @@ function displayBestMoviesCategory(category) {
         document.body.appendChild(categorySection);
         for (i = 0; i < bestMoviesCategory.length - 1; i++) {
             let newCard = document.createElement("div");
-            newCard.className = bestMoviesCategory[i].id;
+            newCard.className = "movieCard";
+            newCard.id = bestMoviesCategory[i].id;
             categorySection.appendChild(newCard)
             let newImg = document.createElement("img");
             let newTitle = document.createElement("a");
@@ -127,8 +101,7 @@ function displayBestMoviesCategory(category) {
             newTitle.innerText = bestMoviesCategory[i].title;
             newTitle.href = "#";
             newTitle.addEventListener("click", function() {
-                console.log(newCard.className);
-                displayModal(newCard.className);
+                displayModal(newCard.id);
             });
             newCard.appendChild(newImg);
             newCard.appendChild(newTitle);
@@ -136,60 +109,26 @@ function displayBestMoviesCategory(category) {
     })
 }
 
-
+// Modal window
 function displayModal(id) {
 
-    // let movieModal = document.getElementById("movieModal");
     let closeModal = document.getElementById("closeModal");
     let dialog = document.getElementById("modal");
-
     let movieImg = document.createElement("img");
-    renderMovieDetails(id).then(movie => {   
+    fetchMovieDetails(id).then(movie => {   
         movieImg.src = movie["image_url"];
     });
     document.querySelector("#modal").appendChild(movieImg);
-
     dialog.showModal();
-
-    // movieModal.addEventListener("click", function() {
-    //     dialog.showModal();
-    // });
     closeModal.addEventListener('click', function() {
         movieImg.remove();
         dialog.close();
     });
 }
 
+// Main
 
-// Data from one specific movie
-
-async function fetchMovieDetails(id) {
-    try {
-        const response = await fetch(`http://localhost:8000/api/v1/titles/${id}`, {
-            method: 'GET',
-            credentials: 'same-origin'
-        });
-        const movie = await response.json();
-        // console.log(movie)
-        return movie;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function renderMovieDetails(id)  {
-    const movie = await fetchMovieDetails(id);
-    // console.log(movie);
-    return movie;   
-}
-
-
-
-
-// displayModal(8571428);
-
-renderBestMovie().then (result => displayBestMovie(result))
-
+fetchBestMoviesId().then (id => displayBestMovie(id));
 displayBestMoviesCategory(category3)
 displayBestMoviesCategory(category2)
 displayBestMoviesCategory(category1)
