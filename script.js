506db819f356dab45117f86ee55bc7b8e7702487
XavierCoulon <xcoulon.mail@gmail.com>
2022-01-14 17:46:26 +0100

    implement 3 categories

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const category1 = "Action";
 const category2 = "Comedy";
 const category3 = "History";
+const scrollWidth = 202;
 const bestMovieSection = document.querySelector(".box-bestmovie");
 const bestMovieDetailsSection = document.querySelector(".bestmovie-details");
 
@@ -45,7 +46,6 @@ async function trailerMovie(id) {
 }
 
 
-
 // Return movies from a category (sorted by score)
 async function fetchBestMoviesByCategory(category) {
     let response = await fetch(`http://localhost:8000/api/v1/titles/?&page_size=8&sort_by=-imdb_score&genre=${category}`);
@@ -59,29 +59,12 @@ function displayBestMovie(id){
     let bestMovieImg = document.createElement("img");
     let bestMovieTitle = document.createElement("li");
     let bestMovieDescription = document.createElement("li");
-    // let bestMovieGenres = document.createElement("li");
-    // let bestMovieDatePublished = document.createElement("li");
-    // let bestMovieVotes = document.createElement("li");
-    // let bestMovieImdbScore = document.createElement("li");
-    // let bestMovieDirectors = document.createElement("li");
-    // let bestMovieActors = document.createElement("li");
-    // let bestMovieDuration = document.createElement("li");
     
     fetchMovieDetails(id).then(movie => {   
         bestMovieImg.src = movie["image_url"];
         bestMovieTitle.innerText = `TITRE: ${movie["title"]}`;
         bestMovieDescription.textContent = `DESCRIPTION: ${movie["description"]}`;
-        // bestMovieSection.id = movie["id"];
-        // bestMovieGenres.textContent = movie["genres"];
-        // bestMovieDatePublished.textContent = movie["date_published"];
-        // bestMovieVotes.textContent = movie["votes"];
-        // bestMovieImdbScore.textContent = movie["imdb_score"];
-        // bestMovieDirectors.textContent = movie["directors"];
-        // bestMovieActors.textContent = movie["actors"];
-        // bestMovieDuration.textContent = movie["duration"];
-        
         document.querySelector(".bestmovie-img").appendChild(bestMovieImg);
-        // bestMovieDetailsSection.appendChild(bestMovieTitle);
         
         trailerMovie(id).then(trailer => {
             document.getElementById("vimeo").src = trailer["linkEmbed"];
@@ -92,13 +75,6 @@ function displayBestMovie(id){
         document.getElementById("button-info").addEventListener("click", function() {
             displayModal(movie["id"]);
         });
-        // bestMovieDetailsSection.appendChild(bestMovieGenres);
-        // bestMovieDetailsSection.appendChild(bestMovieDatePublished);
-        // bestMovieDetailsSection.appendChild(bestMovieVotes);
-        // bestMovieDetailsSection.appendChild(bestMovieImdbScore);
-        // bestMovieDetailsSection.appendChild(bestMovieDirectors);
-        // bestMovieDetailsSection.appendChild(bestMovieActors);
-        // bestMovieDetailsSection.appendChild(bestMovieDuration);
     })
 }
 
@@ -108,7 +84,7 @@ function displayBestMoviesCategory(category, section) {
         let bestMoviesCategory = results;        
         if (section != "bestmovies"){
             document.querySelector(`#${section}_title`).innerText = category;
-        }
+        } 
         for (i = 0; i < bestMoviesCategory.length - 1; i++) {
             let newCard = document.createElement("div");
             newCard.className = "movieCard";
@@ -123,6 +99,7 @@ function displayBestMoviesCategory(category, section) {
             });
         }
     })
+    scrollCarrousel(section);
 }
 
 function displayModal(id) {
@@ -132,12 +109,42 @@ function displayModal(id) {
     let detailsModal = document.querySelector(".modal-details");
     let movieImg = document.createElement("img");
     let movieTitle = document.createElement("li");
+    let movieGenres = document.createElement("li");
+    let movieDatePublished = document.createElement("li");
+    let movieVotes = document.createElement("li");
+    let movieImdbScore = document.createElement("li");
+    let movieDirectors = document.createElement("li");
+    let movieActors = document.createElement("li");
+    let movieDuration = document.createElement("li");
+    let movieCountries = document.createElement("li");
+    let movieBoxOffice = document.createElement("li");
+    let movieDescription = document.createElement("li");
     fetchMovieDetails(id).then(movie => {   
         movieImg.src = movie["image_url"];
         movieTitle.innerText = movie["title"];
+        movieGenres.innerText = movie["genres"];
+        movieDatePublished.innerText = movie["date_published"];
+        movieVotes.innerText = movie["votes"];
+        movieImdbScore.innerText = movie["imdb_score"];
+        movieDirectors.innerText = movie["directors"];
+        movieActors.innerText = movie["actors"];
+        movieDuration.innerText = movie["duration"];
+        movieCountries.innerText = movie["countries"];
+        movieBoxOffice.innerText = movie["worldwide_gross_income"];
+        movieDescription.innerText = movie["description"];
     });
     imgModal.appendChild(movieImg);
     detailsModal.appendChild(movieTitle);
+    detailsModal.appendChild(movieGenres);
+    detailsModal.appendChild(movieVotes);
+    detailsModal.appendChild(movieImdbScore);
+    detailsModal.appendChild(movieDirectors);
+    detailsModal.appendChild(movieActors);
+    detailsModal.appendChild(movieDuration);
+    detailsModal.appendChild(movieCountries);
+    detailsModal.appendChild(movieBoxOffice);
+    detailsModal.appendChild(movieDescription);
+
     modal.style.display = "flex";
     modal.style.position = "fixed";
     modal.style.verticalAlign = "middle";
@@ -148,22 +155,26 @@ function displayModal(id) {
     });
 }
 
+// Scroll element in a category's box
+function scrollCarrousel(category) {
+    document.getElementById(`button-${category}-left`).onclick = function() {
+        document.getElementById(`box-${category}`).scrollLeft -= scrollWidth;
+    }
+    
+    document.getElementById(`button-${category}-right`).onclick = function() {
+        document.getElementById(`box-${category}`).scrollLeft += scrollWidth;
+    }
+}
 
 
 // Main
 
 fetchBestMoviesId().then (id => displayBestMovie(id));
+
+
+
+
 displayBestMoviesCategory("", "bestmovies");
-
-document.getElementById("move-left").onclick = function() {
-    document.getElementById("box-bestmovies").scrollLeft -= 202;
-}
-
-document.getElementById("move-right").onclick = function() {
-    document.getElementById("box-bestmovies").scrollLeft += 202;
-}
-
-
-// displayBestMoviesCategory(category1, "category1");
-// displayBestMoviesCategory(category2, "category2");
-// displayBestMoviesCategory(category3, "category3");
+displayBestMoviesCategory(category1, "category1");
+displayBestMoviesCategory(category2, "category2");
+displayBestMoviesCategory(category3, "category3");
